de775f0ddd72424bf6c37dd250c27e7a254cdd96
Light Helix <107587111+LittMarque27@users.noreply.github.com>
2022-07-31 12:37:23 -0400

    Created object to store question data. Entered HTML placeholders for high-score submission

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,16 @@
-var displayTime = document.querySelector("#time")
-var baseTime = 5
-var startButton = document.querySelector("#start")
+var displayTime = document.querySelector("#time");
+var baseTime = 75;
+var startButton = document.querySelector("#start");
+
+var correctAnswer = ("D. ECMAScript" || "B. HyperText Markup Language" || "B. Selector")
+
+var content = {
+    question: ["What is JavaScript’s official name?", "What does HTML stand for?", "What is used to “find” an HTML element in CSS?"],
+    optionA: ["A. Scripting by Java", "A. Hyper-beam Technique Made Learnable", "A. Locator"],
+    optionB: ["B. VecnaScript", "B. HyperText Markup Language", "B. Selector"],
+    optionC: ["C. jQuery", "C. Heuristic Tool Module Language", "C. Specifier"],
+    optionD: ["D. ECMAScript", "D. Hosted Technology Moderating Language", "D. Parameter"]
+};
 
 function gameTime() {
     var timeInterval = setInterval(function() {
