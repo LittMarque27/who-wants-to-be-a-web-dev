b5ec52d4ab80f8dd0c8f0596b302c0916611194b
Light Helix <107587111+LittMarque27@users.noreply.github.com>
2022-07-31 16:14:05 -0400

    Added functions and event listeners that populate questions and multiple choice fields

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,17 @@
 var displayTime = document.querySelector("#time");
 var baseTime = 75;
 var startButton = document.querySelector("#start");
+var testWrong = document.querySelector("#wrong");
+var testNext = document.querySelector("#next");
+var pullQ = 0;
+
+var questionText = document.querySelector("#q-text");
+var chooseA = document.querySelector("#a")
+var chooseB = document.querySelector("#b")
+var chooseC = document.querySelector("#c")
+var chooseD = document.querySelector("#d")
+
+var checkAnswer = document.querySelectorAll(".press")
 
 var correctAnswer = ("D. ECMAScript" || "B. HyperText Markup Language" || "B. Selector")
 
@@ -19,11 +30,72 @@ function gameTime() {
 
         if(baseTime === 0) {
             clearInterval(timeInterval);
-        }
+        };           
     }, 1000);
-}
+};
 
 startButton.addEventListener("click", function() {
     gameTime();
-}) 
+});
     
+testWrong.addEventListener("click", function() {
+    baseTime -= 10;
+ }); 
+
+function nextQuestion() {
+    console.log(content.question[pullQ]);
+    console.log(content.optionA[pullQ]);
+    console.log(content.optionB[pullQ]);
+    console.log(content.optionC[pullQ]);
+    console.log(content.optionD[pullQ]); 
+    
+    questionText.textContent = (content.question[pullQ]);
+    chooseA.textContent = (content.optionA[pullQ]);
+    chooseB.textContent = (content.optionB[pullQ]);
+    chooseC.textContent = (content.optionC[pullQ]);
+    chooseD.textContent = (content.optionD[pullQ]); 
+};
+
+nextQuestion()
+
+testNext.addEventListener("click", function() {
+    if (pullQ < (content.question.length)) {
+        pullQ++;
+        nextQuestion()
+    }
+});
+
+
+chooseA.addEventListener("click", function() {
+    if (chooseA.value === "D. ECMAScript" || chooseA.value === "B. HyperText Markup Language" || chooseA.value === "B. Selector") {
+        console.log("Correct!");
+    } else {
+        console.log("Incorrect! Lost ten seconds!");
+    }
+})
+
+chooseB.addEventListener("click", function() {
+    if (chooseB.value === "D. ECMAScript" || chooseB.value === "B. HyperText Markup Language" || chooseB.value === "B. Selector") {
+        console.log("Correct!");
+    } else {
+        console.log("Incorrect! Lost ten seconds!");
+    }
+})
+
+chooseC.addEventListener("click", function() {
+    if (chooseC.value === "D. ECMAScript" || chooseC.value === "B. HyperText Markup Language" || chooseC.value === "B. Selector") {
+        console.log("Correct!");
+    } else {
+        console.log("Incorrect! Lost ten seconds!");
+    }
+})
+
+
+chooseD.addEventListener("click", function() {
+    if (chooseD.value === "D. ECMAScript" || chooseD.value === "B. HyperText Markup Language" || chooseD.value === "B. Selector") {
+        console.log("Correct!");
+    } else {
+        console.log("Incorrect! Lost ten seconds!");
+    }
+})
+
